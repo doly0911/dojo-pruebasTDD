7ede943076b45725857b93280f1b910efbeff388
Doly Jimenez <doly.jimenez11@gmail.com>
2022-03-19 12:37:42 -0500

    none any position test, add gitignore file

--- codebreaker.test.js
+++ codebreaker.test.js
@@ -4,3 +4,8 @@ test('full match all digits', () => {
     const result = cb.match('1234'); //Act
     expect(result).toBe('XXXX'); //Assert
 });
+
+test('none in position', () => {
+    const result = cb.match('9578'); 
+    expect(result).toBe(''); 
+});
