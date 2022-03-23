0e40d7d8bd59ceda9ed4f937549a81e9981c9a41
Doly Jimenez <doly.jimenez11@gmail.com>
2022-03-23 18:11:21 -0500

    one and two digits exist but in different location

--- codeBreaker.js
+++ codeBreaker.js
@@ -1,33 +1,34 @@
-function CodeBreaker(secretDigit){
-    this.secretDigit = secretDigit + "";
+function CodeBreaker(secretDigit) {
+  this.secretDigit = secretDigit + "";
 }
 
 const codeBreaker = new CodeBreaker(1234);
-var digitExist = '';
+var digitExist = "";
 
-function match(digits){
+function match(digits) {
+  if (codeBreaker.secretDigit.includes(digits)) {
+    return "XXXX";
+  } else {
+    for (var i = 0; i < 4; i++) {
+      var digit = digits.charAt(i); //separa cada digito ingresado por usuario
+      for (var j = 0; j < 4; j++) {
+        var secretNum = codeBreaker.secretDigit.charAt(j); //separa cada digito del numero a adivinar
 
-    if(codeBreaker.secretDigit.includes(digits)) {
-        return 'XXXX';
-    } 
-     else {
-        for(var i = 0; i < 4; i++){   
-            var digit= digits.charAt(i);  //separa cada digito ingresado por usuario         
-            for (var j=0; j<4; j++){                
-                var secretNum = codeBreaker.secretDigit.charAt(j);   //separa cada digito del numero a adivinar
-
-                if((digit==secretNum) && (i==j)) {
-                    digitExist += 'X';
-                }   
-                j=4;
-            }           
-        }         
-        if (digitExist=='') {
-            return '';
-        } else return digitExist;
-       
+        if (digit == secretNum && i == j) {
+          j = 4;
+          digitExist += "X";
+        } else if (digit == secretNum) {
+          j = 4;
+          digitExist += "-";
+        } else {
+          digitExist += "";
+        }
+      }
     }
-    
+  }
+  rta = digitExist;
+  digitExist = "";
+  return rta;
 }
 
 module.exports.match = match;
--- codebreaker.test.js
+++ codebreaker.test.js
@@ -1,33 +1,41 @@
-const cb = require('./codeBreaker'); 
+const cb = require("./codeBreaker");
 
-test('full match all digits', () => {
-    const result = cb.match('1234'); //Act
-    expect(result).toBe('XXXX'); //Assert
+test("full match all digits", () => {
+  const result = cb.match("1234"); //Act
+  expect(result).toBe("XXXX"); //Assert
 });
 
-test('digits not exist', () => {
-    const result = cb.match('9578'); 
-    expect(result).toBe(''); 
+test("digits not exist", () => {
+  const result = cb.match("9578");
+  expect(result).toBe("");
 });
 
-test('only 1 digit in first position', () => {
-    const result = cb.match('1765'); 
-    expect(result).toBe('X'); 
+test("only 1 digit in first position", () => {
+  const result = cb.match("1765");
+  expect(result).toBe("X");
 });
 
-test('1 digit in the same position but in any location', () => {
-    const result = cb.match('9635'); 
-    expect(result).toBe('X'); 
+test("1 digit in the same position but in any location", () => {
+  const result = cb.match("9635");
+  expect(result).toBe("X");
 });
 
-test('2 digit in the same position but in any location', () => {
-    const result = cb.match('1247'); 
-    expect(result).toBe('XX'); 
+test("3 digit in the same position but in any location", () => {
+  const result = cb.match("1237");
+  expect(result).toBe("XXX");
 });
 
-test('3 digit in the same position but in any location', () => {
-    const result = cb.match('1237'); 
-    expect(result).toBe('XXX'); 
+test("num exist but other location", () => {
+  const result = cb.match("5761");
+  expect(result).toBe("-");
 });
 
+test("2 digit in the same position and 1 digit in any location", () => {
+  const result = cb.match("1247");
+  expect(result).toBe("XX-");
+});
 
+test("2 digit in the same position and 2 digit in any location", () => {
+  const result = cb.match("1243");
+  expect(result).toBe("XX--");
+});
